fb9ab88964d6545ad87150523d0e1d52d2c805e6
Jerry Nabango <jerrynabango9@gmail.com>
2024-01-11 11:19:17 +0300

    list of floats

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from typing import List, Union
+
+""" Complex types - mixed list"""
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Return sum of list of floats"""
+    return sum(mxd_lst)

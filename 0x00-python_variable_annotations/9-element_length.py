0102d5d131b911aa406a723c6cc1361d3d38252d
Jerry Nabango <jerrynabango9@gmail.com>
2024-01-11 12:19:52 +0300

    appropriate types

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-from typing import Iterable, List, Sequence, Tuple
 """Let's duck type an iterable object"""
+from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """return values with the appropriate types"""
     return [(i, len(i)) for i in lst]

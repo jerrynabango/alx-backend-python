8075d4396b6f22bf8f73a6e8367bb28509e8bf55
Jerry Nabango <jerrynabango9@gmail.com>
2024-01-16 09:25:56 +0300

    async_generator that takes no arguments

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Async Generator"""
+    """coroutine called async_generator that takes no arguments"""
     for corot in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
